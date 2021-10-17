9e6ace3f2247425b853fe78fc2302117aa33556e
Desun92 <adri.villa92@gmail.com>
2021-10-17 13:39:07 +0200

    Practica_2

---
+++ app/src/main/java/com/acv/contador/MainActivity.java
@@ -0,0 +1,48 @@
+package com.acv.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int valorContador;
+    private TextView contador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        valorContador = 0;
+        contador = findViewById(R.id.contador);
+
+        findViewById(R.id.incrementar).setOnClickListener(view -> {
+            incrementar();
+        });
+        findViewById(R.id.decrementar).setOnClickListener(view -> {
+            decrementar();
+        });
+        findViewById(R.id.resetear).setOnClickListener(view -> {
+            resetear();
+        });
+
+
+    }
+
+    private void incrementar(){
+        valorContador++;
+        contador.setText(Integer.toString(valorContador));
+    }
+
+    private void decrementar(){
+        valorContador--;
+        contador.setText(Integer.toString(valorContador));
+    }
+
+    private void resetear(){
+        valorContador = 0;
+        contador.setText(Integer.toString(valorContador));
+    }
+}
